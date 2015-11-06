bb992ddd20a4d2f6050dd16e12a1af175dc8186a
TooManyCooks <jakemc77@yahoo.co.uk>
2015-11-06 20:44:19 +0000

    Fixed the goddamn patient-customer relationships

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029162020) do
+ActiveRecord::Schema.define(version: 20151030154614) do
 
   create_table "doctors", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "password"
   end
 
   create_table "patients", force: :cascade do |t|
