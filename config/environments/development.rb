00d62081239e079f038bdec21cfbfc6705651f8e
jtnuim <james.tate.2014@nuim.ie>
2015-12-02 11:24:25 +0000

    Added email functionality, at the moment the email is only sent out on patient creation.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -38,4 +38,21 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+
+
+  #'pmpquizlink@gmail.com', 'teamproject4',
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'example.com',
+      user_name:            'pmpquizlink@gmail.com',
+      password:             'teamproject4',
+      authentication:       'login',
+      enable_starttls_auto: true  }
+
+  #config.action_mailer.default_url_options = { host: "localhost:3000"}
+
 end
