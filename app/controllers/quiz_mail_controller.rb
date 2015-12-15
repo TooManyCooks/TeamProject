fa0d61c2456646c6cf80990b8e6349fb7efb484e
TooManyCooks <jakemc77@yahoo.co.uk>
2015-12-15 22:46:49 +0000

    Fixing email errors

--- app/controllers/quiz_mail_controller.rb
+++ app/controllers/quiz_mail_controller.rb
@@ -1,7 +1,7 @@
 class QuizMailController < ApplicationController
 
   def index
-      PatientMailer.welcome_email('jakemc77@yahoo.co.uk').deliver_now
+      #PatientMailer.welcome_email('jakemc77@yahoo.co.uk').deliver_now
   end
 
 end
